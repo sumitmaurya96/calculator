d26cd7492fcf392144434ea36642cd6f29f080c0
sumitmaurya96 <sumitmaurya96@gmail.com>
2019-09-16 21:27:31 +0530

    Updates

---
+++ precache-manifest.383882b246379c6e20a93a964b49f8ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c64f42c67e0042f9d5914812e65c5619",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "d22ff08b952d365c7b0a",
+    "url": "/calculator/static/css/main.1edf107e.chunk.css"
+  },
+  {
+    "revision": "4d626d92d29118f242ea",
+    "url": "/calculator/static/js/2.ea931c80.chunk.js"
+  },
+  {
+    "revision": "d22ff08b952d365c7b0a",
+    "url": "/calculator/static/js/main.94d77441.chunk.js"
+  },
+  {
+    "revision": "335217aced5d2de02c3f",
+    "url": "/calculator/static/js/runtime~main.7273a9b5.js"
+  }
+]);
